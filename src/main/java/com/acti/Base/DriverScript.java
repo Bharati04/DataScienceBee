ad9ed7e200e673427d0c9c1e08a37128a5c9bd97
Bharati <Bharati@192.168.86.23>
2020-04-30 17:21:50 -0500

    created DriverScript

---
+++ src/main/java/com/acti/Base/DriverScript.java
@@ -0,0 +1,57 @@
+package com.acti.Base;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverScript {
+
+	public static WebDriver driver;// Define global variable
+	public static Properties prop;
+
+	// create a constructor for driver script- because i want to read the
+	// property file first
+	public DriverScript() {
+		try {// where exactly your properties file - src of input
+			File src = new File("./actiConfigeration/config.properties");
+			FileInputStream fis = new FileInputStream(src);
+			// create a object of class
+			prop = new Properties();
+			prop.load(fis);
+
+		}
+
+		catch (Exception e) {
+			System.out.println("Unable to load property file " + e.getMessage());
+		}
+	}
+//@Test // after testing delete it 
+	// create one method
+	public static void initApp() {
+		String browser = prop.getProperty("browser");
+		if (browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "./actiBrowsers/chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		else if (browser.equalsIgnoreCase("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver", "./actiBrowsers/geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(50,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);// for elements on the page
+		
+		String url = prop.getProperty("qaurl");
+		driver.get(url);
+		
+	}
+
+}
